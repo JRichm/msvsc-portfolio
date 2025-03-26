be144589686e6fc7c9727459d0a2e0076216b57e
JRichm <jblackenmusic@gmail.com>
2025-03-25 22:12:09 -0600

    add meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${sourceCodePro.variable} antialiased`}
       >
